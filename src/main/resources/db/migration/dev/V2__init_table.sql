a8fb33eacc5e7e382b511f059fef817cccca5335
Tony Zhu <tony.zhu@sap.com>
2018-01-18 17:51:16 +0800

    [new feature] cloud configuration setup

--- src/main/resources/db/migration/dev/V2__init_table.sql
+++ src/main/resources/db/migration/dev/V2__init_table.sql
@@ -2,7 +2,7 @@ CREATE TABLE business_object (bo_id VARCHAR(255) NOT NULL, last_modified_date TI
 CREATE TABLE business_object_field (field_id VARCHAR(255) NOT NULL, tenant_id VARCHAR(36), last_modified_date TIMESTAMP NOT NULL, created_date TIMESTAMP NOT NULL, creator_uid VARCHAR(255), is_cust_field BOOLEAN, is_multi_input BOOLEAN, is_value_set BOOLEAN, field_name VARCHAR(255) NOT NULL, field_type VARCHAR(255), last_modified_uid VARCHAR(255), bo_id VARCHAR(255), PRIMARY KEY (field_id));
 CREATE TABLE business_object_field_option (field_option_id VARCHAR(255) NOT NULL, tenant_id VARCHAR(36), description VARCHAR(255), field_id VARCHAR(255) NOT NULL, lang_id VARCHAR(255) NOT NULL, value VARCHAR(255) NOT NULL, PRIMARY KEY (field_option_id));
 CREATE TABLE business_object_field_text (field_text_id VARCHAR(255) NOT NULL, tenant_id VARCHAR(36), field_description VARCHAR(255), field_short_description VARCHAR(255), lang_id VARCHAR(255), field_id VARCHAR(255), PRIMARY KEY (field_text_id));
-ALTER TABLE business_object_field ADD CONSTRAINT UNQ_business_object_field_0 UNIQUE (field_name, bo_id);
+ALTER TABLE business_object_field ADD CONSTRAINT UNQ_business_object_field_0 UNIQUE (tenant_id, field_name, bo_id);
 ALTER TABLE business_object_field_option ADD CONSTRAINT UNQ_business_object_field_option_0 UNIQUE (field_id, value, lang_id);
 ALTER TABLE business_object_field_text ADD CONSTRAINT UNQ_business_object_field_text_0 UNIQUE (field_id, lang_id);
 ALTER TABLE business_object_field ADD CONSTRAINT FK_business_object_field_bo_id FOREIGN KEY (bo_id) REFERENCES business_object (bo_id);
